e3a44ce19e6e4d78f4e0b80eeca43fb075060ee9
Jessica Garrido <jessica.garrido.di@gmail.com>
2025-06-25 21:23:48 +0200

    php bin/phpunit tests/Controller/UserControllerTest.php

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -5,27 +5,30 @@ namespace App\Tests\Controller;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 
 final class UserControllerTest extends WebTestCase
 {
-    private $client;
+    private KernelBrowser $client;
     private $container;
+    private $adminUser;
 
     protected function setUp(): void
     {
         parent::setUp();
+
         $this->client = static::createClient();
         $this->container = static::getContainer();
+
+        $userRepository = $this->container->get(UserRepository::class);
+        $this->adminUser = $userRepository->findOneBy(['username' => 'username_1']);
+
+        $this->assertNotNull($this->adminUser, 'L\'utilisateur "username_1" est introuvable.');
     }
 
     public function testListUserAdmin(): void
     {
-        $userRepository = $this->container->get(UserRepository::class);
-
-        $adminUser = $userRepository->findOneBy(['username' => 'username_1']);
-        $this->assertNotNull($adminUser, 'L\'utilisateur "username_1" est introuvable.');
-
-        $this->client->loginUser($adminUser);
+        $this->client->loginUser($this->adminUser);
         $this->client->request('GET', '/users');
 
         $this->assertResponseStatusCodeSame(Response::HTTP_OK);
@@ -34,6 +37,9 @@ final class UserControllerTest extends WebTestCase
 
     public function testUserListRedirectsIfNotAuthenticated(): void
     {
+        // Logout in case previous test left the client authenticated
+        $this->client->request('GET', '/logout'); 
+
         $this->client->request('GET', '/users');
 
         $this->assertResponseRedirects('/login');
@@ -41,96 +47,71 @@ final class UserControllerTest extends WebTestCase
 
     public function testCreateUserFormIsAccessible(): void
     {
-        $userRepository = $this->container->get(UserRepository::class);
-
-        $adminUser = $userRepository->findOneBy(['username' => 'username_1']);
-        $this->assertNotNull($adminUser, 'L\'utilisateur "username_1" est introuvable.');
-
-        $this->client->loginUser($adminUser);
+        $this->client->loginUser($this->adminUser);
         $this->client->request('GET', '/users/create');
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('button', 'Ajouter');
-
     }
 
     public function testCreateUserWithValidData(): void
     {
-    $userRepository = $this->container->get(UserRepository::class);
+        $this->client->loginUser($this->adminUser);
 
-    $adminUser = $userRepository->findOneBy(['username' => 'username_1']);
-    $this->assertNotNull($adminUser, 'L\'utilisateur "username_1" est introuvable.');
+        $crawler = $this->client->request('GET', '/users/create');
 
-    $this->client->loginUser($adminUser);
+        $form = $crawler->selectButton('Ajouter')->form([
+            'user[username]' => 'username_' . uniqid(),
+            'user[password][first]' => 'todo2025',
+            'user[password][second]' => 'todo2025',
+            'user[mail]' => 'new_user@example.com',
+            'user[roles][0]' => 'ROLE_USER',
+        ]);
 
-    $crawler = $this->client->request('GET', '/users/create');
+        $this->client->submit($form);
+        $session = $this->client->getRequest()->getSession();
+        $flashes = $session->getFlashBag()->all();
 
-    $form = $crawler->selectButton('Ajouter')->form([
-        'user[username]' => 'username_'.uniqid(),
-        'user[password][first]' => 'todo2025',
-        'user[password][second]' => 'todo2025',
-        'user[mail]' => 'new_user@example.com',
-        'user[roles][0]' => 'ROLE_USER', 
+        $this->assertArrayHasKey('success', $flashes);
+        $this->assertCount(1, $flashes['success']);
+        $this->assertEquals("L'utilisateur a bien été ajouté.", current($flashes['success']));
+        $this->assertResponseRedirects('/users');
 
-    ]);
+        $this->client->followRedirect();
 
-    $this->client->submit($form);
-    $session = $this->client->getRequest()->getSession();
-    $flashes = $session->getFlashBag()->all();
-
-    $this->assertArrayHasKey('success', $flashes);
-    $this->assertCount(1, $flashes['success']);
-    $this->assertEquals("L'utilisateur a bien été ajouté.", current($flashes['success']));
-    $this->assertResponseRedirects('/users');
-
-    $this->client->followRedirect();
-
-    $this->assertResponseIsSuccessful();
-
-    $this->assertSelectorTextContains('h1', 'Liste des utilisateurs');
-
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Liste des utilisateurs');
     }
 
     public function testEditUserPageAccessible(): void
     {
-        $userRepository = $this->container->get(UserRepository::class);
-
-        $adminUser = $userRepository->findOneBy(['username' => 'username_1']);
-        $this->assertNotNull($adminUser, 'L\'utilisateur "username_1" est introuvable.');
-
-        $this->client->loginUser($adminUser);
+        $this->client->loginUser($this->adminUser);
         $this->client->request('GET', '/users/11/edit');
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('button', 'Modifier');
-
     }
 
     public function testEditUser(): void
     {
-        $userRepository = $this->container->get(UserRepository::class);
-
-        $adminUser = $userRepository->findOneBy(['username' => 'username_1']);
-        $this->assertNotNull($adminUser, 'L\'utilisateur "username_1" est introuvable.');
-        
-        $this->client->loginUser($adminUser);
+        $this->client->loginUser($this->adminUser);
         $crawler = $this->client->request('GET', '/users/11/edit');
 
         $form = $crawler->selectButton('Modifier')->form([
-         'user[username]' => 'username_'.uniqid(),
-         'user[password][first]' => 'todo2025',
-         'user[password][second]' => 'todo2025',
-         'user[mail]' => 'new_user@example.com',
-         'user[roles][1]' => 'ROLE_ADMIN', 
+            'user[username]' => 'username_' . uniqid(),
+            'user[password][first]' => 'todo2025',
+            'user[password][second]' => 'todo2025',
+            'user[mail]' => 'new_user@example.com',
+            'user[roles][1]' => 'ROLE_ADMIN',
+        ]);
 
-     ]);
+        $this->client->submit($form);
 
-     $this->client->submit($form);
+        $this->assertResponseRedirects('/users');
 
-     $this->assertResponseRedirects('/users');
+        $this->client->followRedirect();
 
-     $this->client->followRedirect();
-
-     $this->assertResponseIsSuccessful();
-     $this->assertSelectorTextContains('h1', 'Liste des utilisateurs');}
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Liste des utilisateurs');
+    }
 }
